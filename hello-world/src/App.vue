f9040383778ee75f77152ff79300b8691a8c0e87
waterwatermelon <1044264633@qq.com>
2019-08-06 17:52:51 +0800

    更新：使用xlsx插件，在客户端将excel文件的数据转成json格式

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+    <Upload></Upload>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Upload from './components/Upload.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Upload
   }
 }
 </script>
---
+++ hello-world/src/components/Upload.vue
@@ -0,0 +1,36 @@
+<template>
+    <h4>
+        使用插件xlsx获取excel文件的数据，将其转成json格式
+    </h4>
+    <input type="file" @change="guideUpload"/>
+</template>
+<script>
+import XLSX from 'xlsx';
+export default {
+    methods:{
+        guideUpload(event){
+            console.log('guideload');
+            let file = event.target.files[0];
+            this.filename = file.name;
+            let wb;
+            let rabs = false;
+            const f = file;
+            let reader = new FileReader();
+            reader.onload = e => {
+                let data = e.target.result;
+                console.log(e.target);
+                wb = XLSX.read(data,{
+                    type:'binary'
+                })
+                let excelData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
+                console.log('excelData :', excelData);
+            }
+            if (rabs) {
+                reader.readAsArrayBuffer(f)
+            } else {
+                reader.readAsBinaryString(f)
+            }
+        }
+    }
+}
+</script>
